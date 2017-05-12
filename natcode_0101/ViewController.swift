7370ee8b1cd0aa0ba0c56e2722dafa7994660c77
carlynorama <carlynorama@gmail.com>
2017-05-12 15:29:29 -0700

    Initial Commit

---
+++ natcode_0101/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  natcode_0101
+//
+//  Created by Carlyn Maw on 5/12/17.
+//  Copyright © 2017 carlynorama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
